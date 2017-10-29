e6afb8489c0a85ad14fb1f1a1cbeac220b218533
Guilherme de Oliveira <gui5>
2017-10-28 22:47:18 -0200

    created make file and firts compilation success

--- source/test.cpp
+++ source/test.cpp
@@ -1,15 +1,18 @@
-#include <stdio.h>
+#include <iostream>
+#include "SerialPort.h"
 
 /*
     Command line:
         argv[1] = serial device
         argv[2] = baud rate
+
+        serial --port /dev/ttyUSB0 --baudrate 9600
 */
 
 int main(int argc, char * argv[])
 {
 
-    
+
 
     return 0;
 }
